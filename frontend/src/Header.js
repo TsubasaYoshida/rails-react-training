78a344b1f577db748d0de7cbeff15c4a3785aac4
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-10-08 23:23:24 +0900

    コンポーネントを追加してみる

---
+++ frontend/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function Header() {
+  return (
+    <div className="Header">
+      <header className="App-header">
+        <p>Reactへようこそ！</p>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
