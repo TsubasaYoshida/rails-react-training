72709deb1f7ae57243dba9186861f58a0ee20c3d
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-10-13 00:43:22 +0900

    機密情報を安全に参照できるようにする

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 const CSE_ENDPOINT = 'https://www.googleapis.com/customsearch/v1';
+const GCP_API_KEY = process.env.REACT_APP_GCP_API_KEY;
+const SEARCH_ENGINE_ID = process.env.REACT_APP_SEARCH_ENGINE_ID;
 
 export default class App extends Component {
   constructor(props) {
@@ -13,9 +15,9 @@ export default class App extends Component {
     axios
       .get(CSE_ENDPOINT, {
         params: {
-          key: '',
-          cx: '',
-          q: '',
+          key: GCP_API_KEY,
+          cx: SEARCH_ENGINE_ID,
+          q: '札幌学生野球連盟',
         }
       })
       .then((response) => {
