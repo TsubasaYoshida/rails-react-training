32005f55a5ef1b72dc27acbb8b979232c07a9275
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-10-12 23:53:12 +0900

    Google-CSE-APIを実行してレスポンスを表示する機能を実装する

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,44 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const CSE_ENDPOINT = 'https://www.googleapis.com/customsearch/v1';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleSearch() {
+    axios
+      .get(CSE_ENDPOINT, {
+        params: {
+          key: '',
+          cx: '',
+          q: '',
+        }
+      })
+      .then((response) => {
+          this.setState({
+            title: response.data.items[0].title
+          });
+        },
+      )
+      .catch(() => {
+        console.log('通信に失敗しました。');
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <form>
-          <input type="text" />
-        </form>
+        <h1>検索</h1>
+        <p> {this.state.title} </p>
+        <input
+          type="button"
+          value="検索"
+          onClick={() => this.handleSearch()}
+        />
       </div>
     );
   }
